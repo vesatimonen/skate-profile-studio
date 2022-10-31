fcc0a22e2fde14a72b12121dbe193a6a0a487323
Vesa Timonen <vtimonen@gmail.com>
2022-10-31 10:22:39 +0200

    Remove game board

--- js/UI.js
+++ js/UI.js
@@ -2,7 +2,7 @@
  * UI elements
  *****************************************************************************/
 const appScreen = document.getElementById("app-screen");
-const gameBoard = document.getElementById("game-board");
+const gameBoard = document.getElementById("control-canvas");
 
 /*****************************************************************************
  * Board size variables
@@ -15,13 +15,23 @@ var gameBoardWidth;
 
 
 function uiElementsRedraw() {
-    var canvas = document.getElementById('game-canvas');
+    var canvas = document.getElementById('control-canvas');
     var context = canvas.getContext('2d');
 
     /* Set canvas size and clear it */
-    canvas.width = gameBoard.clientWidth;
-    canvas.height = gameBoard.clientHeight;
-    context.clearRect(0, 0, canvas.width, canvas.height);
+//    canvas.width = gameBoard.clientWidth;
+//    canvas.height = gameBoard.clientHeight;
+//    context.clearRect(0, 0, canvas.width, canvas.height);
+
+//canvas.width = 200;
+//canvas.height = 200;
+
+document.getElementById("debug-text").innerHTML = canvas.width + " / " + canvas.height;
+
+
+    context.moveTo(0, 0);
+    context.lineTo(canvas.width, canvas.height);
+    context.stroke();
 
     context.beginPath();
     context.arc(100, 100, 100, 0, 2 * Math.PI, false);
