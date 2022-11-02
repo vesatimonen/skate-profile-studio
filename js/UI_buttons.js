ca402d5c010f7be3db6c474100f8b00b74f9dcec
Vesa Timonen <vtimonen@gmail.com>
2022-11-02 08:32:00 +0200

    Fine-tune size buttons

--- js/UI_buttons.js
+++ js/UI_buttons.js
@@ -8,6 +8,38 @@ function save(data, filename, type) {
     fileBlob.click();
 }
 
+
+/*****************************************************************************
+ * Size button functions
+ *****************************************************************************/
+const sizeButtons = document.getElementById("size-buttons");
+function uiRedrawSizeButtons() {
+    for (let blade = 0; blade < skateBlades.length; blade++) {
+        var button = document.getElementById("button-" + blade);
+        if (skateBladeSize == skateBlades[blade].size) {
+            button.className = "button button-selected";
+        } else {
+            button.className = "button";
+        }
+    }
+}
+
+function uiInitSizeButtons() {
+    /* Create size buttons */
+    for (let blade = 0; blade < skateBlades.length; blade++) {
+        let newButton = document.createElement("button");
+        if (skateBladeSize == skateBlades[blade].size) {
+            newButton.className = "button button-selected";
+        } else {
+            newButton.className = "button";
+        }
+        newButton.id        = "button-" + blade;
+        newButton.innerHTML = skateBlades[blade].size;
+        newButton.sizeIndex = blade;
+        sizeButtons.appendChild(newButton);
+    }
+}
+
 /*****************************************************************************
  * Button handlers
  *****************************************************************************/
@@ -18,23 +50,17 @@ function uiSave(event) {
     return false;
 }
 
-const sizeButtons = document.getElementById("size-buttons");
-function uiInitSizeButtons() {
-    /* Create size buttons */
-    for (let blade = 0; blade < skateBlades.length; blade++) {
-        let newButton = document.createElement("button");
-        if (skateBladeSize == skateBlades[blade].size) {
-            newButton.className = "button button-selected";
-        } else {
-            newButton.className = "button";
-        }
-        newButton.innerHTML = skateBlades[blade].size;
-        sizeButtons.appendChild(newButton);
-    }
-}
-
 /*****************************************************************************
  * Register button event handlers
  *****************************************************************************/
 document.getElementById("button-save").addEventListener("click",    uiSave);
 
+
+
+function testtest(event) {
+    console.log(event.target.sizeIndex);
+    skateBladeSize = event.target.sizeIndex;
+    uiRedrawSizeButtons();
+}
+// window.addEventListener("click",  testtest);
+document.getElementById("size-buttons").addEventListener("click",  testtest);
