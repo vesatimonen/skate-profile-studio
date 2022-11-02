54eac79b6bf3f20d108328f91acbd73f968cd964
Vesa Timonen <vtimonen@gmail.com>
2022-11-02 07:33:07 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const skateBlades = [
+    {size: 221, length: 65},
+    {size: 230, length: 70},
+    {size: 238, length: 75},
+    {size: 245, length: 80},
+    {size: 254, length: 85},
+    {size: 263, length: 90},
+    {size: 272, length: 95},
+    {size: 280, length: 100},
+    {size: 288, length: 105},
+    {size: 296, length: 110},
+    {size: 306, length: 115},
+];
+
