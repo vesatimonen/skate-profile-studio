93f771f3cf0be2d2a079788859c98a08e53aaec8
Vesa Timonen <vtimonen@gmail.com>
2022-12-09 14:49:40 +0200

    Add image test code

--- js/main.js
+++ js/main.js
@@ -16,3 +16,48 @@ var skateBladeIndex = 6;
 
 uiInitControls();
 uiInitSizeButtons();
+
+
+
+
+const skateImage   = document.getElementById('skate-image');
+const imageCanvas  = document.getElementById("image-canvas");
+const imageContext = imageCanvas.getContext('2d');
+
+//skateImage.src = URL.createObjectURL("images/icon.png");
+skateImage.src = "images/test.jpg";
+
+
+skateImage.onload = function () {
+    imageCanvas.width = this.width;
+    imageCanvas.height = this.height;
+    imageCanvas.crossOrigin = "anonymous";
+
+imageCanvas.width = 300;
+imageCanvas.height = 300;
+
+    var hRatio = imageCanvas.width  / skateImage.width;
+    var vRatio = imageCanvas.height / skateImage.height;
+    var ratio  = Math.min(hRatio, vRatio);
+    var centerShift_x = ( imageCanvas.width  - skateImage.width * ratio ) / 2;
+    var centerShift_y = ( imageCanvas.height - skateImage.height * ratio ) / 2;
+    imageContext.drawImage(skateImage, 0, 0, skateImage.width, skateImage.height,                                           // Source rectangle
+                                       centerShift_x,centerShift_y, skateImage.width * ratio, skateImage.height * ratio);   // Target rectangle
+
+
+    imageContext.beginPath();
+    imageContext.moveTo(0, 0);
+    imageContext.lineTo(200, 200);
+    imageContext.fillStyle   = "none";
+    imageContext.lineWidth   = 2;
+    imageContext.lineCap     = "round";
+    imageContext.strokeStyle = "#303030";
+    imageContext.stroke();
+};
+
+
+
+
+
+
+
