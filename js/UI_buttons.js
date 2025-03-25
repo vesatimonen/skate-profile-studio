9fc1bb241737292b1200b145052703f31f82cedc
Vesa Timonen <vtimonen@gmail.com>
2025-03-25 08:22:29 +0200

    Save point

--- js/UI_buttons.js
+++ js/UI_buttons.js
@@ -21,15 +21,27 @@ function uiExportSVG() {
 function uiExportDataPointsTxt() {
 
     let dataPoints = [
-        {x: 1.0, y: 2.0},
+        {x: 1.3, y: 2.0},
         {x: 2.0, y: 4.0},
         {x: 3.0, y: 6.0}
     ];
 
     console.log(dataPoints);
+
+    const csvContent = dataPoints.map(obj => Object.values(obj).join(" ")).join("\n");
+    console.log(csvContent);
+
+
+    var fileData = new Blob([csvContent], {type: "text/plain"});
+    var fileBlob = document.getElementById("export-profile");
+
+    fileBlob.href     = URL.createObjectURL(fileData);
+    fileBlob.download = document.getElementById("profile-name").value + ".txt";
+
 }
 
 function uiExport() {
+
     var profileType = document.getElementById("profile-type").value;
     switch (profileType) {
         case "data-points-txt":
